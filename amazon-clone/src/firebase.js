acaafff30f419a7e67d380f02ee5afdd82267e38
Damola Olusakin <olusakinadedamola@gmail.com>
2020-11-21 20:07:16 +0100

    create app

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDnSJ1nOdicumZ3EeAlwF3YgPucIPiJS9M",
+    authDomain: "challenge-adf09.firebaseapp.com",
+    databaseURL: "https://challenge-adf09.firebaseio.com",
+    projectId: "challenge-adf09",
+    storageBucket: "challenge-adf09.appspot.com",
+    messagingSenderId: "379513530647",
+    appId: "1:379513530647:web:a105ecdf9119810439ef52"
+  };
