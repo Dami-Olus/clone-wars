9391f89be19c98528e08d9a23f9d17ecef059fb0
Damola Olusakin <olusakinadedamola@gmail.com>
2020-11-22 08:58:09 +0100

    add the nav bar options

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
       <h1>Hello Clever Programmers, Let's build the Amazon Store!</h1>
+      <Header/>
+      {/*Home*/}
     </div>
   );
 }
