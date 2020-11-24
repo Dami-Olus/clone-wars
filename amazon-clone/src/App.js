458ee724034f1fffc818cf83e40572cc1fae3b84
Damola Olusakin <olusakinadedamola@gmail.com>
2020-11-24 23:44:00 +0100

    style navbar and header

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -5,7 +5,6 @@ import Header from './Header';
 function App() {
   return (
     <div className="app">
-      <h1>Hello Clever Programmers, Let's build the Amazon Store!</h1>
       <Header/>
       {/*Home*/}
     </div>
