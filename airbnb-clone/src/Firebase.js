d5642803814f6698364f9b502ad4637e1d6f1131
Damola Olusakin <olusakinadedamola@gmail.com>
2020-12-05 15:45:47 +0100

    add firebase

---
+++ airbnb-clone/src/Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCxkh84NTgo5dAMob5_yLbEyGoDCZRQBxw",
+    authDomain: "airbnb-clone-322a9.firebaseapp.com",
+    databaseURL: "https://airbnb-clone-322a9.firebaseio.com",
+    projectId: "airbnb-clone-322a9",
+    storageBucket: "airbnb-clone-322a9.appspot.com",
+    messagingSenderId: "256972684541",
+    appId: "1:256972684541:web:3932b7d2157e2933c41294"
+  };
