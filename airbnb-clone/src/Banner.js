0ffae2b41c92c54ab7c2f33017cc1579d79094ee
Damola Olusakin <olusakinadedamola@gmail.com>
2020-12-05 23:08:37 +0100

    add date search component

--- airbnb-clone/src/Banner.js
+++ airbnb-clone/src/Banner.js
@@ -1,15 +1,16 @@
 import { Button } from '@material-ui/core';
 import React, {useState} from 'react';
 import './Banner.css';
+import Search from './Search';
 
 function Banner() {
     const [showSearch, setShowSearch] = useState(false);
 
     return (
         <div className = 'banner' >
-            {showSearch  && <h1>SHOW DATE PICKER</h1>}
             <div className = 'banner__search'>
-                <Button onClick={()=>setShowSearch(!showSearch)} className = 'banner__searchButton' variant='outlined'>
+            {showSearch  && <Search />}
+                <Button onClick={() => setShowSearch(!showSearch)} className = 'banner__searchButton' variant='outlined'>
                     Search Dates
                 </Button>
             </div>
