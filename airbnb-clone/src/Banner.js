5d2533db8ca6ae715ee1ba471ba65321b470bf02
Damola Olusakin <olusakinadedamola@gmail.com>
2020-12-05 21:39:37 +0100

    add state for date pciker button

--- airbnb-clone/src/Banner.js
+++ airbnb-clone/src/Banner.js
@@ -1,10 +1,18 @@
 import { Button } from '@material-ui/core';
-import React from 'react';
+import React, {useState} from 'react';
 import './Banner.css';
 
 function Banner() {
+    const [showSearch, setShowSearch] = useState(false);
+
     return (
         <div className = 'banner' >
+            {showSearch  && <h1>SHOW DATE PICKER</h1>}
+            <div className = 'banner__search'>
+                <Button onClick={()=>setShowSearch(!showSearch)} className = 'banner__searchButton' variant='outlined'>
+                    Search Dates
+                </Button>
+            </div>
             <div className = 'banner__info'>
                 <h1>Get out and stretch your imagination</h1>
                 <h5> Plan a different kind of getaway to uncover the hidden gems near you</h5>
