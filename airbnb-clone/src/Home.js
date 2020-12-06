4013b3baaaae2cf7af81641aeba3da8cbbdf201b
Damola Olusakin <olusakinadedamola@gmail.com>
2020-12-06 10:23:28 +0100

    add first card info

--- airbnb-clone/src/Home.js
+++ airbnb-clone/src/Home.js
@@ -9,7 +9,11 @@ function Home() {
             <Banner/>
 
             <div className = 'home__section'>
-                <Card />
+                <Card 
+                src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
+                title="Online Experiences"
+                description="Unique activities we can do together, led by a world of hosts"
+                />
                 <Card />
                 <Card />
             </div>
