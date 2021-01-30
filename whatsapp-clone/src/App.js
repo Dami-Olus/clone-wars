2f7a6ace423ccff56ef9c127618a1c2a55ab26b1
Damola Olusakin <olusakinadedamola@gmail.com>
2021-01-30 01:29:15 +0100

    create sidebar and chat component

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Chat from './Chat';
+import Sidebar from './Sidebar';
+
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets build a whatsapp clonme</h1>
+      
+      <Sidebar />
+      <Chat />
     </div>
   );
 }
