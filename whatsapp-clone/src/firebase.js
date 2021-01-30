64a0617e430dc5161a9331c469bfdf0ffac2caac
Damola Olusakin <olusakinadedamola@gmail.com>
2021-01-30 01:17:05 +0100

    add firebase config

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB9zbXj8HsfBdmQZCkz7UQFg0cRRXoJvzU",
+  authDomain: "whatsapp-clone-59951.firebaseapp.com",
+  projectId: "whatsapp-clone-59951",
+  storageBucket: "whatsapp-clone-59951.appspot.com",
+  messagingSenderId: "249381768514",
+  appId: "1:249381768514:web:075b32f5b95e2bacac0cdb"
+};
