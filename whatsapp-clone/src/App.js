3616f76da9d503c0ba0695697f68bee2f2e49f66
Damola Olusakin <olusakinadedamola@gmail.com>
2021-01-30 02:04:59 +0100

    add icons and avatar from material ui

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
@@ -6,9 +7,11 @@ import Sidebar from './Sidebar';
 function App() {
   return (
     <div className="app">
-      
+      <div className="app__body">
       <Sidebar />
       <Chat />
+      </div>
+     
     </div>
   );
 }
--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import "./Sidebar.css";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import { Avatar, IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>I am a sidebar</h1>
+      <div className="sidebar__header">
+        
+        <Avatar src="https://media-exp1.licdn.com/dms/image/C4E03AQHyjO2ULGrgYQ/profile-displayphoto-shrink_400_400/0/1605250333227?e=1617235200&v=beta&t=xw78_oAZVoEyJYrYDkYozOx7dKgiDULqBmVhP4HMOeQ"/>
+
+        <div className="sidebar__headerRight">
+          <IconButton>
+          <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+          <ChatIcon />
+          </IconButton>
+          <IconButton>
+          <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
