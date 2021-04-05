18aadce865ccb2d3cf9b9c6d98f86e0e7f11f840
Damola Olusakin <olusakinadedamola@gmail.com>
2021-04-05 20:50:12 +0100

    add header icons and avatar

--- linkedin-clone/src/Header.js
+++ linkedin-clone/src/Header.js
@@ -4,6 +4,9 @@ import SearchIcon from '@material-ui/icons/Search';
 import HeaderOption from './HeaderOption';
 import HomeIcon from "@material-ui/icons/Home";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
   return (
@@ -25,7 +28,11 @@ function Header() {
 <div className="header__right">
    <HeaderOption Icon={HomeIcon} title="Home"/>
    <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
-</div>
+   <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+   <HeaderOption Icon={ChatIcon} title="Messaging" />
+   <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+   <HeaderOption avatar="https://compassionate-leakey-e9b16b.netlify.app/immages/IG_Sonny.jpeg" title="me" />
+</div> 
 
     </div>
   );
