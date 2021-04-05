9097dbf22e9409b5cdc1c79550aa3244264cce1d
Damola Olusakin <olusakinadedamola@gmail.com>
2021-04-05 16:00:26 +0100

    add header left icons

--- linkedin-clone/src/Header.js
+++ linkedin-clone/src/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'; 
 import "./Header.css";
 import SearchIcon from '@material-ui/icons/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 
 function Header() {
   return (
     <div className='header'>
-      <h1>This is the header </h1>
+      
 
     <div class="header__left">
       <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt=""/>
@@ -20,11 +23,12 @@ function Header() {
 
 
 <div className="header__right">
-
+   <HeaderOption Icon={HomeIcon} title="Home"/>
+   <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
 </div>
 
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
